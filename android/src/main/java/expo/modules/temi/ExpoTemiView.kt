e03577666db6ad7a9ef9f030fed26abe4598fce5
Strengthless <dickykam3@gmail.com>
2023-06-18 00:39:04 +0800

    Configure project to android only

--- android/src/main/java/expo/modules/temi/ExpoTemiView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.temi
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoTemiView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
